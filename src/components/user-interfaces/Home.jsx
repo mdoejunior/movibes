1aa9fa0eb65a9892b2265c659e816baaa610f7a7
mdoejunior <issamdoe2019@gmail.com>
2023-01-15 23:25:02 +0300

    url image link

--- src/components/user-interfaces/Home.jsx
+++ src/components/user-interfaces/Home.jsx
@@ -15,7 +15,7 @@ const Home = ()=>{
         <div className='flex flex-row justify-around'>
             <div>
                 <div className='bg-white rounded-3xl overflow-hidden shadow-lg mt-10 relative'>
-                    <img src='./src/assets/moon.jpg' alt='' className='w-full h-72'/>
+                    <img src='https://alkony.enerla.net/images/film/Moonfall-2022-1.jpg' alt='' className='w-full h-72'/>
                     <div className='flex absolute gap-4 bottom-3 right-0'>
                         <button type="button" className="w-40 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-button-color dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                                 <FontAwesomeIcon icon={faPlay} className='text-2xl mr-3'/><span className='text-lg'>Play</span>
